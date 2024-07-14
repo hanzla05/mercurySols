466fd8daca162814f8c58f2c051058b3a2cfecac
Hanzla Munir <imhanzla05@gmail.com>
2024-07-14 17:37:26 +0500

    index

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
-import app from "./api";
+import express from 'express';
+import './db.mjs'; // Ensure this file establishes the connection to MongoDB
+import register from './Controller/register.mjs';
+import login from './Controller/login.mjs';
+import User from './models/user.mjs';
+import getallUsers from './Controller/getallUsers.mjs';
+import deleteuserbyid from './Controller/deleteuserbyid.mjs';
 
-export default async (req, res) => {
-    app(req, res);
-};
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req,res)=>{
+res.send("Hello World")
+});
+
+
+app.post('/register', register);
+app.post('/login', login);
+app.get('/users',getallUsers);
+app.delete('/users/:id',deleteuserbyid);
+
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
